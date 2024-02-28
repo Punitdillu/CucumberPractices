ab087908a75106c6bd490da3e020dd330a785cd7
Punitdillu <punitranjan2105@gmail.com>
2024-02-28 07:57:38 +0530

    features file added in resouce folder

--- src/test/java/cucumberOption/TestRunner.java
+++ src/test/java/cucumberOption/TestRunner.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/features",glue ="stepDefinition"
+@CucumberOptions(features="src/test/resources/features",glue ="stepDefinition"
 ,monochrome=true, tags ="@OffersPage or @SearchAndAddToCart or @SearchOnly",
 plugin= {"html:target/cucumber.html", "json:target/cucumber.json",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
