84ebfb6819b4ed5069924b3832d6d7d6bf4c9a88
Punitdillu <punitranjan2105@gmail.com>
2026-02-09 21:52:56 +0530

    files updated

--- src/test/java/cucumberOption/TestRunner.java
+++ src/test/java/cucumberOption/TestRunner.java
@@ -5,15 +5,15 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/resources/features",glue ="stepDefinition"
-,monochrome=true, tags ="@OffersPage or @SearchAndAddToCart or @SearchOnly",
+@CucumberOptions(features="src/test/resources/features",glue = {"stepDefinition"}
+		,monochrome=true, tags ="@OffersPage or @SearchAndAddToCart or @SearchOnly",
 plugin= {"html:target/cucumber.html", "json:target/cucumber.json",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 		"rerun:target/failed_scenarios.txt"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 	@Override
-	@DataProvider(parallel=false)
+	@DataProvider(parallel=true)
 	public Object[][] scenarios()
 	{
 		return super.scenarios();
